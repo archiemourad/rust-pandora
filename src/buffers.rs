523b7d21e8dc9ff2bdbb60b227664781fe763549
Archie Mourad <mouradarchie+github@gmail.com>
2024-01-02 12:27:23 +1000

    added 'buffers.rs': 'Buffer(1D)', 'Buffer2D'

---
+++ src/buffers.rs
@@ -0,0 +1,37 @@
+use std::ops::{Index, IndexMut};
+
+pub struct Buffer<T> {
+    pub data: Vec<T>,
+}
+
+impl<T> Index<usize> for Buffer<T> {
+    type Output = T;
+
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
+    }
+}
+
+impl<T> IndexMut<usize> for Buffer<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index]
+    }
+}
+
+pub struct Buffer2D<T> {
+    pub data: Vec<Vec<T>>,
+}
+
+impl<T> Index<(usize, usize)> for Buffer2D<T> {
+    type Output = T;
+
+    fn index(&self, index: (usize, usize)) -> &Self::Output {
+        &self.data[index.0][index.1]
+    }
+}
+
+impl<T> IndexMut<(usize, usize)> for Buffer2D<T> {
+    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
+        &mut self.data[index.0][index.1]
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,25 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+mod buffers;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_buffers() {
+        use crate::buffers::{Buffer, Buffer2D};
+
+        let mut buffer = Buffer::<f32> {
+            data: vec![1.0, 2.0, 3.0],
+        };
+
+        assert_eq!(buffer[0], 1.0);
+        buffer[0] = 2.0;
+        assert_eq!(buffer[0], 2.0);
+
+        let mut buffer2d = Buffer2D::<f32> {
+            data: vec![vec![1.0, 2.0, 3.0]],
+        };
+
+        assert_eq!(buffer2d[(0, 0)], 1.0);
+        buffer2d[(0, 0)] = 2.0;
+        assert_eq!(buffer2d[(0, 0)], 2.0);
     }
 }
