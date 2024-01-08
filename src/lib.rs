89513d4fa9d5b1c5bcf07727ffac2a8666fff565
Archie Mourad <mouradarchie+github@gmail.com>
2024-01-08 10:49:01 +1000

    cleaned tests in 'tests'

--- src/lib.rs
+++ src/lib.rs
@@ -5,23 +5,6 @@ pub mod shader;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_buffers() {
-        use crate::buffers::{Buffer, Buffer2D};
-
-        let mut buffer = Buffer::new(vec![1.0, 2.0, 3.0]);
-
-        assert_eq!(buffer[0], 1.0);
-        buffer[0] = 2.0;
-        assert_eq!(buffer[0], 2.0);
-
-        let mut buffer2d = Buffer2D::new(vec![vec![1.0, 2.0, 3.0]]);
-
-        assert_eq!(buffer2d[(0, 0)], 1.0);
-        buffer2d[(0, 0)] = 2.0;
-        assert_eq!(buffer2d[(0, 0)], 2.0);
-    }
-
-    #[test]
     fn test_shader() {
         use crate::shader::Shader;
 
@@ -42,19 +25,7 @@ mod tests {
 
     #[test]
     fn test_raster() {
-        use crate::raster::{edge_function, inside_triangle, Point2D};
-
-        let start = Point2D { x: 0.0, y: 0.0 };
-        let end = Point2D { x: 0.0, y: 1.0 };
-
-        assert_eq!(
-            edge_function(&start, &end, &Point2D { x: 1.0, y: 0.5 }),
-            true
-        );
-        assert_eq!(
-            edge_function(&start, &end, &Point2D { x: -1.0, y: 0.5 }),
-            false
-        );
+        use crate::raster::{inside_triangle, Point2D};
 
         let triangle = [
             Point2D { x: -0.5, y: 0.0 },
@@ -66,6 +37,7 @@ mod tests {
             inside_triangle(&triangle, &Point2D { x: 0.0, y: 0.5 }),
             true
         );
+
         assert_eq!(
             inside_triangle(&triangle, &Point2D { x: 0.0, y: 2.0 }),
             false
