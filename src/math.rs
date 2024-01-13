1729c0130d16f16500fef2da2484a0f5ab76e6d6
Archie Mourad <mouradarchie+github@gmail.com>
2024-01-13 11:17:18 +1000

    added 'Point3D' to 'math.rs'

--- src/math.rs
+++ src/math.rs
@@ -3,3 +3,10 @@ pub struct Point2D {
     pub x: f64,
     pub y: f64,
 }
+
+/// A three-dimensional non-generic point (struct) with an `x` (x-coordinate), `y` (y-coordinate) and `z` (z-coordinate)
+pub struct Point3D {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
