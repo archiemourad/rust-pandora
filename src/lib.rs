31d7b7692385faa53a93e19d9aa9dbc201e377d9
Archie Mourad <mouradarchie+github@gmail.com>
2024-01-18 16:10:56 +1000

    'shader.rs' to 'proxy.rs': 'Shader' to 'Proxy'

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,27 @@
 pub mod buffers;
 pub mod math;
+pub mod proxy;
 pub mod raster;
-pub mod shader;
 
 #[cfg(test)]
 mod tests {
     #[test]
-    fn test_shader() {
-        use crate::shader::Shader;
+    fn test_proxy() {
+        use crate::proxy::Proxy;
 
         struct IO {
             x: f32,
         }
 
-        fn shader_function(io: &mut IO) -> IO {
+        fn proxy_function(io: &mut IO) -> IO {
             IO { x: io.x + 2.0 }
         }
 
-        let mut shader = Shader::new(shader_function);
+        let mut proxy = Proxy::new(proxy_function);
 
         let mut input = IO { x: 1.0 };
 
-        assert_eq!((shader.function)(&mut input).x, 3.0);
+        assert_eq!((proxy.function)(&mut input).x, 3.0);
     }
 
     #[test]
--- src/proxy.rs
+++ src/proxy.rs
@@ -1,13 +1,13 @@
 use std::boxed::Box;
 
 /// A generic function-proxy (struct) with an `IT` (InputType) and `OT` (OutputType)
-pub struct Shader<IT, OT> {
+pub struct Proxy<IT, OT> {
     pub function: Box<dyn FnMut(&mut IT) -> OT>,
 }
 
-impl<IT, OT> Shader<IT, OT> {
+impl<IT, OT> Proxy<IT, OT> {
     pub fn new(function: impl FnMut(&mut IT) -> OT + 'static) -> Self {
-        Shader {
+        Proxy {
             function: Box::new(function),
         }
     }
