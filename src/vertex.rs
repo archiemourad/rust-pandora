1baf2071972afdb232dd6abf5e5bc9c36510b539
Archie Mourad <81056435+archiemourad@users.noreply.github.com>
2024-01-20 09:41:18 +1000

    '(optional)' to 'Vertex' for clarity

--- src/vertex.rs
+++ src/vertex.rs
@@ -1,6 +1,6 @@
 use crate::math::Point3D;
 
-/// A semi-generic vertex (struct) with `attributes` of type `A` and a `point` (position)
+/// A semi-generic vertex (struct) with (optional) `attributes` of type `A` and a `point` (position)
 pub struct Vertex<A> {
     pub point: Point3D,
     pub attributes: Option<A>,
