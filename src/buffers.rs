6f84758492b53dbd143368bbd536cb62dbc3d5fb
Archie Mourad <mouradarchie@gmail.com>
2024-03-21 14:24:58 +1000

    modified indexing for 'Buffer2D'

--- src/buffers.rs
+++ src/buffers.rs
@@ -38,17 +38,17 @@ impl<T> Buffer2D<T> {
     }
 }
 
-impl<T> Index<(usize, usize)> for Buffer2D<T> {
-    type Output = T;
+impl<T> Index<usize> for Buffer2D<T> {
+    type Output = [T];
 
-    fn index(&self, index: (usize, usize)) -> &Self::Output {
-        &self.data[index.0][index.1]
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.data[index]
     }
 }
 
-impl<T> IndexMut<(usize, usize)> for Buffer2D<T> {
-    fn index_mut(&mut self, index: (usize, usize)) -> &mut Self::Output {
-        &mut self.data[index.0][index.1]
+impl<T> IndexMut<usize> for Buffer2D<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.data[index]
     }
 }
 
